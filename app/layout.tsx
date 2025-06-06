81c70b6eb46eedf4f78b66d4584784b9971b1770
SarabjitSingh18 <166316546+SarabjitSingh18@users.noreply.github.com>
2025-06-07 00:14:19 +0530

    fixing issue on dep

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navigation } from "@/components/Navbar";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { ThemeProvider } from "@/components/theme-provider";
 
 
 const geistSans = Geist({
